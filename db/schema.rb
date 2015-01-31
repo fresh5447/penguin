ebc6454c53a0516579850f81f61881a048d267d1
Doug Walter <doug@kosmojo.com>
2015-01-30 18:34:15 -0700

    prep

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141203222236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "students", force: true do |t|
+    t.string   "name"
+    t.string   "favorite_food"
+    t.string   "music"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
